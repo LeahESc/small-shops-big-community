65de356d35114e47c23a54075a978eca1151dc52
LeahESc <leahemily1@gmail.com>
2021-01-29 09:21:22 -0800

    sets up model relationships

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,2 +1,5 @@
 class Shop < ApplicationRecord
+    belongs_to :category
+    has_many :shop_tags 
+    has_many :tags, through: :shop_tags
 end
--- app/serializers/tag_serializer.rb
+++ app/serializers/tag_serializer.rb
@@ -1,4 +1,5 @@
 class TagSerializer < ActiveModel::Serializer
   attributes :id, :name
   has_many :shop_tags
+  has_many :shops, through: :shop_tags
 end
